20512867a0789f8227804282d9c166724f2d7bb0
Eduardo Perez Leale <eduardo.perez@nextgames.com>
2020-10-09 18:40:20 +0300

    Upload all the files that are necessary for the game to work.

---
+++ Assets/_Complete-Game/Tests/PlayerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PlayerTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator PlayerTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+        
+        // TODO: add test cases
+        // TODO: test 1 when player moves lose energy
+        // TODO: test 2 when player moves movement list is updated.
+        // TODO: add shadow.
+    }
+}
