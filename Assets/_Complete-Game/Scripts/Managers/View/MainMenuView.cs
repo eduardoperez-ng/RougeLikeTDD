e7871a014a9b2a3b8cf591149f360015061f8992
Eduardo Perez Leale <eduardo.perez@nextgames.com>
2020-10-30 21:53:03 +0200

    Add a new main view and MainMenu scene.

---
+++ Assets/_Complete-Game/Scripts/Managers/View/MainMenuView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuView : MonoBehaviour
+{
+    public Button startGameButton;
+
+    private void Start()
+    {
+        if (startGameButton != null)
+        {
+            startGameButton.onClick.AddListener(StartGame);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("Main");
+    }
+}
